0273d823b7e1bed328aa8ac0c6f9a57f0fa518de
Orestis Tsirakis <orestistsira@gmail.com>
2025-08-23 14:20:21 +0300

    added channel to read bytes from file

--- main.go
+++ main.go
@@ -4,7 +4,49 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		buffer := make([]byte, 8)
+		currentLine := ""
+
+		for {
+			n, err := f.Read(buffer)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				fmt.Println("Error reading file:", err)
+				return
+			}
+
+			parts := strings.Split(string(buffer[:n]), "\n")
+
+			if len(parts) == 1 {
+				currentLine += parts[0]
+			} else if len(parts) == 2 {
+				out <- (currentLine + parts[0])
+				currentLine = parts[1]
+			} else {
+				fmt.Println("Error: More than 2 split parts found")
+				return
+			}
+		}
+
+		if currentLine != "" {
+			out <- currentLine
+		}
+	}()
+
+	return out
+}
 
 func main() {
 	file, err := os.Open("messages.txt")
@@ -12,20 +54,9 @@ func main() {
 		fmt.Println("Error opening file:", err)
 		return
 	}
-	defer file.Close()
 
-	buffer := make([]byte, 8)
-
-	for {
-		n, err := file.Read(buffer)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Println("Error reading file:", err)
-			return
-		}
-
-		fmt.Printf("read: %s\n", string(buffer[:n]))
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
